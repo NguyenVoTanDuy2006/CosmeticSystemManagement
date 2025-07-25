8edd40af2f5b90cc34a3aa874abbf3f80eda270f
Nguyễn Võ Tấn Duy <nguyenvotanduy2006@gmail.com>
2025-07-25 15:14:32 +0700

    fix input method

--- Cosmetic/productIO.cpp
+++ Cosmetic/productIO.cpp
@@ -1,24 +1,21 @@
 #include "product.hpp"
 
-istream &operator>>(istream &is, Product &ojb) {
+istream& operator>>(istream& is, Product& ojb)
+{
     is >> ojb.info;
     LotInfo temp;
-
-    while (is >> temp) {
-        if (temp.quantity == -1) break;
+    while (is >> temp)
         ojb.Shipments.push_back(temp);
-    }
     if (ojb.ID == "") ojb.ID = ojb.CreateID();
     return is;
 }
 
-ostream &operator<<(ostream &os, const Product &ojb) {
+ostream& operator<<(ostream& os, const Product& ojb)
+{
     if (ojb.ID == "")
         return (os << "null", os);
-
-    os << ojb.ID << "\n"
-            << ojb.info << '\n';
-    for (auto shipment: ojb.Shipments)
+    os << ojb.info << '\n';
+    for (auto shipment : ojb.Shipments)
         os << shipment << '\n';
     return os;
 }
