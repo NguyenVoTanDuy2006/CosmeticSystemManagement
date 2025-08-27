c0f00cc1133877523b106f81d3c4e4b0eb3fd4c1
Nguyễn Võ Tấn Duy <nguyenvotanduy2006@gmail.com>
2025-08-27 11:27:09 +0700

    fix bug

--- CosmeticManagement/saveProductsData.cpp
+++ CosmeticManagement/saveProductsData.cpp
@@ -1,26 +1,31 @@
 #include "productManager.h"
 
-bool productManager::saveData() {
-    removeProduct(""); // Keep logic if necessary
-
+bool productManager::saveData()
+{
     QFile productListFile(getPath("ListProducts"));
-    if (!productListFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+    if (!productListFile.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
         qWarning() << "Could not open ListProducts for writing.";
         return false;
     }
     QTextStream productListStream(&productListFile);
 
-    for (const auto& product : products) {
-        if (product.getInfo().name.isEmpty()) continue; // Skip dummy product
-        productListStream << product.ID << '\n'; // Assuming product ID is name
+    for (const auto& product : products)
+    {
+        QString pID = product.getID();
+        if (pID.isEmpty()) continue; // Skip dummy product
+        productListStream << product.ID << '\n';
 
-        QFile productDataFile(getPath(product.getInfo().name));
-        if (productDataFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QFile productDataFile(getPath(pID));
+        if (productDataFile.open(QIODevice::WriteOnly | QIODevice::Text))
+        {
             QTextStream productDataStream(&productDataFile);
             productDataStream << product;
             productDataFile.close();
-        } else {
-            qWarning() << "Could not open" << getPath(product.getInfo().name) << "for writing.";
+        }
+        else
+        {
+            qWarning() << "Could not open" << getPath(pID) << "for writing.";
         }
     }
     productListFile.close();
