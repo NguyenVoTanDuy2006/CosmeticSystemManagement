c8e58a376612eb528a6ee4d9acf8262c3e712067
Nguyễn Võ Tấn Duy <nguyenvotanduy2006@gmail.com>
2025-08-31 23:25:08 +0700

    fix bug

--- BACKEND/TradeManagement/TradeFactory.h
+++ BACKEND/TradeManagement/TradeFactory.h
@@ -8,15 +8,19 @@
 class TradeFactory
 {
 public:
-    // ID có dạng yyyyMMddhhmmss_SPxxxxx_T, T là type
-    static std::shared_ptr<Trade> getTrade(const QString& ID) {
-        if (ID.endsWith("_0")) {
-             return std::make_shared<TradeIn>(ID);
-        } else if (ID.endsWith("_1")) {
+    // ID có dạng yyyyake_shared<TradMMddhhmmss_SPxxxxx_T, T là type
+    static std::shared_ptr<Trade> getTrade(const QString &ID)
+    {
+        if (ID.endsWith("_0"))
+        {
+            return std::make_shared<TradeIn>(ID);
+        }
+        else if (ID.endsWith("_1"))
+        {
             return std::make_shared<TradeOut>(ID);
         }
         return nullptr;
     }
 };
 
-#endif //TRADEFACTORY_H
+#endif // TRADEFACTORY_H
